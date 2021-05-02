af70325773532c56256b624910cbeec63ac4bec3
Jordan Baumgardner <developer@jordanbaumgardner.com>
2021-05-02 14:49:25 -0600

    Initial Commit

---
+++ RadarMap/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RadarMap
+//
+//  Created by Jordan Baumgardner on 5/2/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RadarMap/RadarMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  RadarMapApp.swift
+//  RadarMap
+//
+//  Created by Jordan Baumgardner on 5/2/21.
+//
+
+import SwiftUI
+
+@main
+struct RadarMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
